a21d4e0444571cdd7405e117334652919f446cc6
Patrick Coelho <criapix@gmail.com>
2018-10-08 20:35:09 -0300

    Atualizacao

--- interno/js/app.js
+++ interno/js/app.js
@@ -1,3 +1,22 @@
+// FLOAT ACTION BUTTON
 $('.fab').click(function() {
   $(this).toggleClass('open');
 });
+
+// DROPDOWN CUSTOM
+var dropdown = document.querySelectorAll('.custom-dropdown');
+dropdown.forEach(function(item){
+  item.querySelector('a').addEventListener('click', function(event) {
+    event.preventDefault();
+    var parent = item.parentNode;
+    var child = parent.querySelector('.custom-dropdown-menu');
+    if(child.style.display == 'block') {
+      child.style.display = 'none';
+      this.classList.remove('active');
+    } else {
+      child.style.display = 'block';
+      this.classList.add('active');
+    }
+  });
+});
+
